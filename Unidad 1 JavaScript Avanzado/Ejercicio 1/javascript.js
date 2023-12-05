8c49d823ae20e921b61c8e88293a2d4166a9b934
nicolasmatiasmm <141781747+nicolasmatiasmm@users.noreply.github.com>
2023-12-05 14:53:12 -0300

    Add files via upload
    Tarea Unidad 1 JavaScript Avanzado

---
+++ Unidad 1 JavaScript Avanzado/Ejercicio 1/javascript.js
@@ -0,0 +1,22 @@
+function cambiarColorFondo(){
+    let body = document.getElementsByTagName('Body')[0];
+    body.style.backgroundColor = 'red';
+}
+
+
+function cambiarColorFondo2(){
+    let body = document.getElementsByTagName('Body')[0];
+    body.style.backgroundColor = 'blue';
+}
+
+
+
+function cambiarColorFondo3(){
+    let body = document.getElementsByTagName('Body')[0];
+    body.style.backgroundColor = 'green';
+}
+
+function cambiarColorReset(){
+    let body = document.getElementsByTagName('Body')[0];
+    body.style.backgroundColor = 'white';
+}
---
+++ Unidad 1 JavaScript Avanzado/Ejercicio 2/script.2.js
@@ -0,0 +1,6 @@
+let contador = 0;
+
+
+function countChars(obj){
+    document.getElementById("contador").innerHTML = obj.value.length;
+}
