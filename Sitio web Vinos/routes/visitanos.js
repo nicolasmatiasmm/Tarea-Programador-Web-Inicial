d45d467a30604e07b090c275a9fa775b9751e9f7
nicolasmatiasmm <141781747+nicolasmatiasmm@users.noreply.github.com>
2024-01-19 16:58:42 -0300

    Sitio web Vinos - Formulario
    Unidad 3 - Envio de Formulario

---
+++ Sitio web Vinos/routes/visitanos.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var router = express.Router();
+var nodemailer = require("nodemailer");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("visitanos");
+});
+
+
+
+
+router.post("/visitanos", async (req, res, next) => {
+  console.log(req.body);
+
+  var nombre = req.body.nombre;
+  var telefono = req.body.telefono;
+  var email = req.body.email;
+
+  var obj = {
+    to: "nicolasmurruni@gmail.com",
+    subject: "Contacto desde la Web",
+    html: nombre + "" + "" + telefono + "" + email,
+  };
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  var info = await transport.sendMail(obj);
+
+  res.render("visitanos", {
+    message: "Mensaje enviado correctamente",
+  });
+});
+
+module.exports = router;
